bf908314cf38b333946a7bccf8e2bdf31aa3477e
ilanazare <ivanazare94@gmail.com>
2025-10-19 09:57:55 -0300

    includes adjustments to the database configuration

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 import os
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
@@ -6,23 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://user:password@localhost:5432/customer_db"
-)
+    "DATABASE_URL", "postgresql+asyncpg://user:password@localhost/customer_db")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
 AsyncSessionLocal = sessionmaker(
-    engine,
+    bind= engine,
     class_= AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
+    autoflush=False,
 )
 
 Base = declarative_base()
 
-async def get_db():
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
